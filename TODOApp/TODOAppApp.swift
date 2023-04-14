49110f3543bb15ffb174cba73edf06f5936e6618
TasukuFujiwara <tasuku0221@icloud.com>
2023-04-14 16:09:01 +0900

    Initial Commit

---
+++ TODOApp/TODOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOAppApp.swift
+//  TODOApp
+//
+//  Created by 藤原輔 on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct TODOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
