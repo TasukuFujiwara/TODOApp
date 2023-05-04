a22721bf270eb2f3a41d710a16657089df26d0a5
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-04 13:44:02 +0900

    Implemented sample CategoryView(side menu): it appears by tapping CategoryButton under CreateButton or swiping MainView from left to right.

---
+++ TODOApp/Views/CategoryView.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryView.swift
+//  TODOApp
+//
+//  Created by 藤原輔 on 2023/05/04.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
--- TODOApp/Views/MainView.swift
+++ TODOApp/Views/MainView.swift
@@ -17,103 +17,132 @@ struct MainView: View {
     @State fileprivate var isEditing: Bool = false          // アイテムを選択できる状態かどうか
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                List {
-                    ForEach(viewModel.todoItems.freeze()) { item in
-                        if !isEditing {     // アイテム選択可能状態でない時
-                            NavigationLink {
-                                EditView(id: item.id, title: item.title, dueDate: item.dueDate, note: item.note)        // 詳細・編集画面へ遷移
-                                    .environmentObject(ViewModel())
-                            } label: {
-                                Text(item.title)      // ラベルとして，タイトルのみ表示しておく
-                            }
-                        } else {            // アイテム選択可能状態である時
-                            HStack {
-                                Text(item.title)
-                                Spacer()
-                                ZStack {
-                                    if selectedItemID.contains(item.id) {   // アイテムが選択されていたら
-                                        // チェックマークをつける
-                                        Image(systemName: "circle.fill")
-                                            .foregroundColor(Color.blue)
-                                            .imageScale(/*@START_MENU_TOKEN@*/.medium/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            let xOffset = geometry.size.width * 0.65
+            NavigationStack {
+                ZStack {
+                    CategoryView()
+                        //.frame(width: xOffset)
+                        //.animation(.easeInOut(duration: 0.5), value: categoryView)
+                    
+                    List {
+                        ForEach(viewModel.todoItems.freeze()) { item in
+                            if !isEditing {     // アイテム選択可能状態でない時
+                                NavigationLink {
+                                    EditView(id: item.id, title: item.title, dueDate: item.dueDate, note: item.note)        // 詳細・編集画面へ遷移
+                                        .environmentObject(ViewModel())
+                                } label: {
+                                    Text(item.title)      // ラベルとして，タイトルのみ表示しておく
+                                }
+                            } else {            // アイテム選択可能状態である時
+                                HStack {
+                                    Text(item.title)
+                                    Spacer()
+                                    ZStack {
+                                        if selectedItemID.contains(item.id) {   // アイテムが選択されていたら
+                                            // チェックマークをつける
+                                            Image(systemName: "circle.fill")
+                                                .foregroundColor(Color.blue)
+                                                .imageScale(/*@START_MENU_TOKEN@*/.medium/*@END_MENU_TOKEN@*/)
+                                        }
+                                        Image(systemName: "circle")
+                                            .foregroundColor(Color.gray)
+                                            .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
                                     }
-                                    Image(systemName: "circle")
-                                        .foregroundColor(Color.gray)
-                                        .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
-                                }
 
-                            }
-                            .contentShape(Rectangle())
-                            .onTapGesture {         // アイテムをタップした時
-                                withAnimation {
-                                    if selectedItemID.contains(item.id) {       // アイテムが選択されていれば，チェックを外す
-                                        selectedItemID.remove(item.id)
-                                    } else {        // アイテムが選択されていなければ，チェックをつける
-                                        selectedItemID.insert(item.id)
+                                }
+                                .contentShape(Rectangle())
+                                .onTapGesture {         // アイテムをタップした時
+                                    withAnimation {
+                                        if selectedItemID.contains(item.id) {       // アイテムが選択されていれば，チェックを外す
+                                            selectedItemID.remove(item.id)
+                                        } else {        // アイテムが選択されていなければ，チェックをつける
+                                            selectedItemID.insert(item.id)
+                                        }
                                     }
                                 }
                             }
+                        }   // ForEach
+                    }   // List
+                    .onTapGesture {
+                        if categoryView {
+                            categoryView = false
                         }
-
                     }
-                }
-                .navigationTitle("TODOリスト")
-                .navigationBarTitleDisplayMode(.large)
-                // ツールバー
-                .toolbar {
-                    ToolbarItem(placement:.navigationBarTrailing) {
-                        Button(isEditing ? "削除" : "選択") {  // アイテムが1つ以上選択されていれば，押せる状態に
-                            if isEditing {
-                                for id in selectedItemID {      // 選択したアイテムを削除
-                                    viewModel.deleteItem(id)
-                                    selectedItemID.remove(id)
+                    .navigationTitle(categoryView ? "" : "TODOリスト")
+                    .navigationBarTitleDisplayMode(.large)
+                    .offset(x: categoryView ? xOffset : 0)
+                    .animation(.easeInOut(duration: 0.3), value: categoryView)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                let offset = value.translation.width
+                                if offset > 0 {
+                                    categoryView = true
                                 }
-                                if viewModel.todoItems.isEmpty {     // TODOアイテムがなくなったら，選択可能状態を終了
-                                    isEditing = false
+                            }
+                            .onEnded { value in
+                                let offset = value.translation.width
+                                if offset < xOffset / 2 {
+                                    categoryView = false
                                 }
-                            } else {
-                                isEditing.toggle()
+                            }
+                    )
+                    // ツールバー
+                    .toolbar {
+                        ToolbarItem(placement:.navigationBarTrailing) {
+                            Button(isEditing ? "削除" : "選択") {  // アイテムが1つ以上選択されていれば，押せる状態に
+                                if isEditing {
+                                    for id in selectedItemID {      // 選択したアイテムを削除
+                                        viewModel.deleteItem(id)
+                                        selectedItemID.remove(id)
+                                    }
+                                    if viewModel.todoItems.isEmpty {     // TODOアイテムがなくなったら，選択可能状態を終了
+                                        isEditing = false
+                                    }
+                                } else {
+                                    isEditing.toggle()
+                                }
+                            }
+                            .disabled((isEditing && selectedItemID.isEmpty) || viewModel.todoItems.isEmpty)
+                        }
+                        if isEditing {
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                // 選択可能状態なら，”キャンセル”，そうでなければ，”選択”
+                                // TODOアイテムが1つ以上ある場合に押せる
+                                Button("キャンセル") {
+                                    selectedItemID = []
+                                    isEditing.toggle()
+                                }
+                                .disabled(viewModel.todoItems.isEmpty)
                             }
                         }
-                        .disabled(viewModel.todoItems.isEmpty)
-                    }
-                    if isEditing {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            // 選択可能状態なら，”キャンセル”，そうでなければ，”選択”
-                            // TODOアイテムが1つ以上ある場合に押せる
-                            Button("キャンセル") {
-                                isEditing.toggle()
-                            }
-                            .disabled(viewModel.todoItems.isEmpty)
-                        }
-                    }
-
-                }
-                // 新規作成ボタン
-                VStack {
-                    Spacer()
-                    HStack {
+                    }   // .toolbar
+                    
+                    // 新規作成ボタン
+                    VStack {
                         Spacer()
-                        if !isEditing {     // 選択可能状態でないなら，表示
-                            VStack {
-                                CreateButton(createView: $createView)
-                                    .padding()
-                                CategoryButton(categoryView: $categoryView)
+                        HStack {
+                            Spacer()
+                            if !isEditing {     // 選択可能状態でないなら，表示
+                                VStack {
+                                    CreateButton(createView: $createView)
+                                        .padding()
+                                    CategoryButton(categoryView: $categoryView)
+                                }
                             }
                         }
                     }
-                }
+                }   // ZStack
+            }   // NavigationStack
+            // モーダル遷移で新規作成画面へ
+            .sheet(isPresented: $createView) {
+                CreateView()
+                    .environmentObject(ViewModel())
             }
-        }
-        // モーダル遷移で新規作成画面へ
-        .sheet(isPresented: $createView) {
-            CreateView()
-                .environmentObject(ViewModel())
-        }
-    }
-}
+        }   // GeometryReader
+    }   // body
+}   // MainView
 
 // 新規作成ボタン
 struct CreateButton: View {
