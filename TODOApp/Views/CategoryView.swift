4e9d893b4d763ecab7aa2841cd5f449d024e7059
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-06 17:10:24 +0900

    Modified CategoryView: implemented sample buttons

--- TODOApp/Views/CategoryView.swift
+++ TODOApp/Views/CategoryView.swift
@@ -10,14 +10,33 @@ import SwiftUI
 
 struct CategoryView: View {
     @Environment(\.colorScheme) var colorScheme
+    @EnvironmentObject var viewModel: ViewModel
+    @Binding var nowCategory: String
+    @Binding var categoryView: Bool
     
     var body: some View {
         let textColor: Color = colorScheme == .light ? .black : .white
         NavigationStack {
             ZStack(alignment: .leading) {
                 Color.blue
-                Text("Hello, World!")
+                VStack {
+                    Button("全てのTODO") {
+                        nowCategory = "なし"
+                        categoryView = false
+                    }
                     .padding()
+                    ForEach(Array(viewModel.categoryList), id: \.self) { key in
+                        
+                        Button(key) {
+                            nowCategory = key
+                            categoryView = false
+                        }
+                        .padding()
+
+                    }
+                        
+                }
+
             }
             .edgesIgnoringSafeArea(.all)
             .foregroundColor(textColor)
@@ -27,6 +46,7 @@ struct CategoryView: View {
 
 struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryView()
+        CategoryView(nowCategory: Binding.constant("なし"), categoryView: Binding.constant(true))
+            .environmentObject(ViewModel())
     }
 }
