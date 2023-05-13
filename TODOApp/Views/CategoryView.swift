bd2a07ea0ca3181a538034ea6f6c5cd8a2b4131b
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-13 16:34:55 +0900

    Create CreateNewFolderView

--- TODOApp/Views/CategoryView.swift
+++ TODOApp/Views/CategoryView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct CategoryView: View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var viewModel: ViewModel
+    
+    @State var createNewFolder: Bool = false
+    
     @Binding var nowCategory: String
     @Binding var categoryView: Bool
     
@@ -18,33 +21,77 @@ struct CategoryView: View {
     
     var body: some View {
         let textColor: Color = colorScheme == .light ? .black : .white
-        NavigationStack {
-            ZStack(alignment: .leading) {
-                Color.blue
-                VStack {
-                    Button("全てのTODO") {
-                        nowCategory = "なし"
-                        categoryView = false
-                    }
-                    .padding()
-                    ForEach(categoryList, id: \.self) { key in
-                        
-                        Button(key) {
-                            nowCategory = key
+        GeometryReader { geometry in
+            //let width = geometry.size.width, height = geometry.size.height
+            NavigationStack {
+                ZStack(alignment: .leading) {
+                    Color.blue
+                    VStack {
+                        Button("全てのTODO") {
+                            nowCategory = "なし"
                             categoryView = false
                         }
                         .padding()
+                        ForEach(viewModel.categories, id: \.self) { key in
+                            
+                            Button(key) {
+                                nowCategory = key
+                                categoryView = false
+                            }
+                            .padding()
 
-                    }
+                        }
                         
-                }
+                        Button("+ 新規フォルダ") {
+                            createNewFolder.toggle()
+                        }
+                        .padding()
+                            
+                    }
 
+                }
+                .edgesIgnoringSafeArea(.all)
+                .foregroundColor(textColor)
+            }   // NavigationStack
+            .fullScreenCover(isPresented: $createNewFolder) {
+                CreateNewFolderView(createNewFolder: $createNewFolder)
             }
-            .edgesIgnoringSafeArea(.all)
-            .foregroundColor(textColor)
-        }   // NavigationStack
+        }   // GeometryReader
     }   // body
 }   // CategoryView
+
+struct CreateNewFolderView: View {
+    @Environment(\.isPresented) var isPresented
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var newFolderName = ""
+    
+    @Binding var createNewFolder: Bool
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("新規フォルダ名", text: $newFolderName)
+            }
+            .toolbar {
+                if isPresented {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("キャンセル") {
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("追加") {
+                            // カテゴリーリストに追加
+                            // 既に同名のフォルダが存在する場合，追加せずにエラーメッセージを出す
+                            dismiss()
+                        }
+                    }
+                }   // if isPresented
+            }   // toolbar
+        }   // NavigationStack
+    }
+}
 
 struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
