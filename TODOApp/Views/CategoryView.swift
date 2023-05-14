5acac5f2edb5cfd31b7d2a4bf0fcdbf34f21703b
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-14 16:38:44 +0900

    Deleted Button & View about CreateNewFolder

--- TODOApp/Views/CategoryView.swift
+++ TODOApp/Views/CategoryView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 
 struct CategoryView: View {
@@ -23,7 +24,6 @@ struct CategoryView: View {
     var body: some View {
         let textColor: Color = colorScheme == .light ? .black : .white
         GeometryReader { geometry in
-            //let width = geometry.size.width, height = geometry.size.height
             NavigationStack {
                 ZStack(alignment: .leading) {
                     Color.blue
@@ -34,86 +34,22 @@ struct CategoryView: View {
                                 nowCategory = "全てのTODO"
                                 categoryView = false
                             }
-                        ForEach(viewModel.categories, id: \.self) { key in
+                        ForEach(viewModel.categorySet.elements, id: \.self) { key in
                             Text(key)
                                 .onTapGesture {
                                     nowCategory = key
                                     categoryView = false
                                 }
-                                .contextMenu {
-                                    Button(action: {
-                                        viewModel.deleteCategory(key)
-                                    }, label: {
-                                        Text("削除")
-                                            .foregroundColor(.red)
-                                    })
-                                }
                                 .padding()
                         }
-                        Button("+ 新規フォルダ") {
-                            createNewFolder = true
-                        }
-                        .padding()
                     }   // VStack
                 }   // ZStack
                 .edgesIgnoringSafeArea(.all)
                 .foregroundColor(textColor)
             }   // NavigationStack
-            .fullScreenCover(isPresented: $createNewFolder) {
-                CreateNewFolderView(createNewFolder: $createNewFolder)
-                    .environmentObject(ViewModel())
-            }
         }   // GeometryReader
     }   // body
 }   // CategoryView
-
-struct CreateNewFolderView: View {
-    @Environment(\.isPresented) var isPresented
-    @Environment(\.dismiss) var dismiss
-    
-    @EnvironmentObject var viewModel: ViewModel
-    
-    @State private var newFolderName = ""
-    @State private var displayError = false
-    
-    @Binding var createNewFolder: Bool
-
-    var body: some View {
-        NavigationStack {
-            Form {
-                TextField("新規フォルダ名", text: $newFolderName)
-            }
-            .toolbar {
-                if isPresented {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button("キャンセル") {
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("追加") {
-                            if !viewModel.categories.contains(newFolderName) {
-                            // カテゴリーリストに追加
-                                if displayError {
-                                    displayError = false
-                                }
-                                viewModel.addCategory(newFolderName)
-                                dismiss()
-                            } else {
-                             // 既に同名のフォルダが存在する場合，追加せずにエラーメッセージを出す
-                                displayError = true
-                            }
-                        }
-                    }
-                }   // if isPresented
-            }   // toolbar
-            if displayError {
-                Text("Folder name: \(newFolderName) already exists")
-                    .foregroundColor(.red)
-            }
-        }   // NavigationStack
-    }
-}
 
 struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
