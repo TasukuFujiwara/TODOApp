495fa2e302f744fc1da81d500f865dad77848e55
TasukuFujiwara <tasuku0221@icloud.com>
2023-05-14 16:39:58 +0900

    Implemented ErrorMessage

--- TODOApp/Views/CreateView.swift
+++ TODOApp/Views/CreateView.swift
@@ -6,6 +6,24 @@
 //
 
 import SwiftUI
+import RealmSwift
+
+enum Error: LocalizedError {
+    case none
+    case nameIsEmpty
+    case folderAlreadyExists
+    
+    var errorDescription: String? {
+        switch self {
+        case .none:
+            return nil
+        case .nameIsEmpty:
+            return "新しいフォルダ名を入力してください"
+        case .folderAlreadyExists:
+            return "そのフォルダ名は既に作成済みです"
+        }
+    }
+}
 
 // 新規作成画面
 struct CreateView: View {
@@ -14,10 +32,14 @@ struct CreateView: View {
     @State private var dueDate: Date = Date()
     @State private var note: String = ""
     @State private var category: String = "なし"
+    @State private var tmpCategory: String = ""
+    
+    @State fileprivate var displayError: Error = .none
+    
     @Environment(\.dismiss) var dismiss                 // 遷移元に戻る
     @Environment(\.isPresented) var isPresented         // 他の画面から遷移してきたかどうか
     
-    let sampleCategories = ["なし", "仕事", "重要"]
+    @Binding var nowCategory: String
     
     var body: some View {
         NavigationStack {
@@ -27,11 +49,19 @@ struct CreateView: View {
                 DatePicker("期日", selection: $dueDate)
                     .datePickerStyle(.compact)
                 Picker("フォルダ", selection: $category) {
-                    let categories = ["なし"] + viewModel.categories
+                    let categories = ["なし"] + viewModel.categorySet + ["+ 新規フォルダ"]
                     ForEach(categories, id: \.self) { key in
                         Text(key).tag(key)
                     }
                 }
+                .onAppear {
+                    category = nowCategory == "全てのTODO" ? "なし" : nowCategory
+                }
+                
+                if category == "+ 新規フォルダ" {
+                    TextField("新規フォルダ名", text: $tmpCategory)
+                }
+                
                 TextField("メモ", text: $note)
                     .frame(height: 200)
             }
@@ -48,24 +78,37 @@ struct CreateView: View {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         // データをデータベースに追加．遷移元に戻る
                         Button("追加") {
-                            viewModel.addItem(
-                                title: title == "" ? "New Title" : title,
-                                dueDate: dueDate,
-                                note: note,
-                                category: category
-                            )
-                            dismiss()
+                            if category == "+ 新規フォルダ" && tmpCategory == "" {
+                                //displayError = true
+                                displayError = .nameIsEmpty
+                            } else if category == "+ 新規フォルダ" && viewModel.categorySet.contains(tmpCategory) {
+                                displayError = .folderAlreadyExists
+                            } else {
+                                viewModel.addItem(
+                                    title: title == "" ? "New Title" : title,
+                                    dueDate: dueDate,
+                                    note: note,
+                                    category: category == "+ 新規フォルダ" ? tmpCategory : (category == "" ? "なし" : category)
+                                )
+                                dismiss()
+                            }
                         }
                     }
                 }   // if isPresented
             }   // toolbar
+
+            if let errorMessage = displayError.errorDescription {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+            }
+
         }   // NavigationStack
     }   // body
 }   // CreateView
 
 struct CreateView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateView()
+        CreateView(nowCategory: Binding.constant("なし"))
             .environmentObject(ViewModel())
     }
 }
